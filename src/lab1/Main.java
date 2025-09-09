17dc8fb775db8b313996e0a02cb716f81fdb19da
Kimberly Prijadi <prijadikimberly@gmail.com>
2025-09-09 12:20:06 -0400

    created Main.java

---
+++ src/lab1/Main.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+//        FizzBuzz fb = new FizzBuzz();
+        FizzBuzz.main(args); // runs main algo
+        FizzBuzz.doFizzBuzz(5); // runs doFizzBuzz only
+    }
+}
