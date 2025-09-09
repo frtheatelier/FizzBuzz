a3b3c721a4066d6b4157a3f4053f7feaf9ed4a00
Kimberly Prijadi <prijadikimberly@gmail.com>
2025-09-09 12:45:36 -0400

    refactored Multiples.java to include the default case

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,6 +2,7 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        System.out.println(multiples());
         System.out.println(multiples(1000, 3, 5));
     }
 
@@ -9,6 +10,16 @@ public class Multiples {
         int total = 0;
         for (int i = 1; i < n; i++) {
             if (i % a == 0 || i % b == 0) {
+                total++;
+            }
+        }
+        return total;
+    }
+
+    public static int multiples() {
+        int total = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
                 total++;
             }
         }
